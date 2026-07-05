efedf60878bb9e9bbd6c54e87139fa47c3dcf404
Brandon W. King <70168+kingb@users.noreply.github.com>
2026-07-05 01:29:30 -0700

    ember 0.1.0: per-arch cask (on_arm arm64 / on_intel x86_64)

--- Casks/ember.rb
+++ Casks/ember.rb
@@ -1,9 +1,19 @@
 cask "ember" do
   version "0.1.0"
-  sha256 "7725538b70de1333bf15f81a6b3da2c2eb553262b8300be8559bd50a4dd78ddc"
 
-  url "https://github.com/kingb/ember/releases/download/v#{version}/Ember-#{version}.zip",
-      verified: "github.com/kingb/ember/"
+  on_arm do
+    sha256 "7725538b70de1333bf15f81a6b3da2c2eb553262b8300be8559bd50a4dd78ddc"
+
+    url "https://github.com/kingb/ember/releases/download/v#{version}/Ember-#{version}.zip",
+        verified: "github.com/kingb/ember/"
+  end
+  on_intel do
+    sha256 "2b3f8e9922e2fd0748e583e25bb073d97eed59c90cdec9a7399a8629291e076d"
+
+    url "https://github.com/kingb/ember/releases/download/v#{version}/Ember-#{version}-x86_64.zip",
+        verified: "github.com/kingb/ember/"
+  end
+
   name "Ember"
   desc "GPU-accelerated campfire terminal emulator"
   homepage "https://emberterm.com/"
