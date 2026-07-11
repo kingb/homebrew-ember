dcba590a4df590192b1cf584eb83c86cc292e5a4
Brandon W. King <70168+kingb@users.noreply.github.com>
2026-07-10 23:55:25 -0700

    ember 0.4.2 (cask: per-arch sha256)

--- Casks/ember.rb
+++ Casks/ember.rb
@@ -1,14 +1,14 @@
 cask "ember" do
-  version "0.4.1"
+  version "0.4.2"
 
   on_arm do
-    sha256 "d3eca0158282e575c8c2744a34443dac258ebe937073c98d7d70950887980db0"
+    sha256 "0c1f0f8134d0afeb351e0993515283336c615327bac66c583839057742e3ad9c"
 
     url "https://github.com/kingb/ember/releases/download/v#{version}/Ember-#{version}-macos-arm64.zip",
         verified: "github.com/kingb/ember/"
   end
   on_intel do
-    sha256 "10da0ccb3aa7b4f92505004cf7e1ddbfc90851891ac33cf64f5b27eb31265633"
+    sha256 "e58ca69bcb4abebcbc8902af32c20753536635aec9129dd02c8f44d218f93d3f"
 
     url "https://github.com/kingb/ember/releases/download/v#{version}/Ember-#{version}-macos-x86_64.zip",
         verified: "github.com/kingb/ember/"
